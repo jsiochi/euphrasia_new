3d98133656a25373833031a5da4c08fcf2462cad
Jeremiah Siochi <jeremiahsiochi@gmail.com>
2013-10-23 23:09:07 -0400

    I added two new files, the contract and database helper java files. also updated the DatabaseHelper by adding extra fields (the extra Field subclasses still need to be created) and methods

--- src/com/jbj/euphrasia/Controller.java
+++ src/com/jbj/euphrasia/Controller.java
@@ -1,12 +1,14 @@
 package com.jbj.euphrasia;
 
+import android.content.Context;
+
 public class Controller {
 
 	private EntryDatabaseManager myEntry;
 	private RecordingManager myRecordingManager;
 
-	public Controller() {
-		myEntry = new EntryDatabaseManager();
+	public Controller(Context context) {
+		myEntry = new EntryDatabaseManager(context);
 		myRecordingManager = new RecordingManager();
 	}
 	
--- src/com/jbj/euphrasia/EntryDatabaseManager.java
+++ src/com/jbj/euphrasia/EntryDatabaseManager.java
@@ -1,19 +1,50 @@
 package com.jbj.euphrasia;
 
+import android.content.Context;
+
 public class EntryDatabaseManager {
 	
 	private Field myForeignText;
 	private Field myNativeText;
 	private Field myAudioField;
+	private Field myDateField;
+	private Field myTagField;
 	
-	public EntryDatabaseManager(){
+	private EntryDatabaseHelper myDatabaseHelper;
+	
+	/**
+	 * @param context is the current context, found by calling getContext()
+	 */
+	public EntryDatabaseManager(Context context){
 		/*
 		 * Set up a new object
+		 * (You gotta set 'em up kid
+		 * set 'em up)
 		 */
+		
+		myDatabaseHelper = new EntryDatabaseHelper(context);
+		
+		//TODO instantiate all the Field subclasses
+	}
+	
+	public void saveEntry() {
+		/*
+		 * Write current entry to database
+		 */
+		
+		//TODO add a new database entry
 	}
 
 	public Field getNativeText() {
 		return myNativeText;
+	}
+	
+	public Field getForeignText() {
+		return myForeignText;
+	}
+	
+	public Field getAudioField() {
+		return myAudioField;
 	}
 
 	public void setNativeText(Field data) {
