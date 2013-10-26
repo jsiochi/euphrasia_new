9fae786bf40044715dc7156a33c91c1ce2a48ea5
James Flynn <jtflynnz@live.com>
2013-10-25 23:14:38 -0400

    Added layouts to activity_main and activity_entry.

--- gen/com/jbj/euphrasia/R.java
+++ gen/com/jbj/euphrasia/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int edit_tags=0x7f080001;
+        public static final int edit_title=0x7f080000;
+        public static final int foreign_text=0x7f080003;
+        public static final int native_text=0x7f080002;
     }
     public static final class layout {
         public static final int activity_entry=0x7f030000;
@@ -37,8 +41,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int button_record_begin=0x7f050007;
+        public static final int button_start=0x7f050004;
+        public static final int edit_entry_name=0x7f050005;
+        public static final int edit_entry_tags=0x7f050006;
+        public static final int hint_entry_foreign=0x7f05000b;
+        public static final int hint_entry_native=0x7f05000a;
+        public static final int text_entry_foreign=0x7f050009;
+        public static final int text_entry_native=0x7f050008;
         public static final int title_activity_entry=0x7f050003;
+        public static final int welcome_greeting=0x7f050002;
     }
     public static final class style {
         /** 
