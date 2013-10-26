0506a2ad9f67071610d2a5e6c07f0eee68c0cf9f
Jeremiah Siochi <jeremiahsiochi@gmail.com>
2013-10-26 01:52:58 -0400

    database thread updated

--- src/com/jbj/euphrasia/DatabaseWriteIntentService.java
+++ src/com/jbj/euphrasia/DatabaseWriteIntentService.java
@@ -1,18 +1,29 @@
 package com.jbj.euphrasia;
 
+import com.jbj.euphrasia.EntryContract.EntryColumns;
+
 import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 
 public class DatabaseWriteIntentService extends IntentService {
-	public DatabaseWriteIntentService() {
+	Context myContext;
+	
+	public DatabaseWriteIntentService(Context context) {
 		super("DatabaseWriteIntentService");
+		myContext = context;
 	}
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		// TODO Auto-generated method stub
-		// EntryDatabaseHelper myDatabaseHelper = new EntryDatabaseHelper(intent.
-		// SQLiteDatabase database = EntryDatabaseHelper
+		EntryDatabaseHelper myDatabaseHelper = new EntryDatabaseHelper(myContext);
+		SQLiteDatabase database = myDatabaseHelper.getWritableDatabase();
+		ContentValues values = intent.getParcelableExtra("values");
+		long newRowId;
+		newRowId = database.insert(EntryColumns.TABLE_NAME, EntryColumns.COLUMN_NAME_NULLABLE, values);
 	}
 	
 	
