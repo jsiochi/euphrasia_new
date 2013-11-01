61c0f621c9369e30b9e99281d925c360217ead09
Brad Sykes <bradleysykes1@gmail.com>
2013-11-01 14:49:47 -0400

    Added search activity, page navigation, beginning ActionBar

--- src/com/jbj/euphrasia/MainActivity.java
+++ src/com/jbj/euphrasia/MainActivity.java
@@ -2,10 +2,14 @@ package com.jbj.euphrasia;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
+
 import com.jbj.euphrasia.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,16 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
+	
+	public void onStartEntry(View view){
+		startActivity(new Intent(this, EntryActivity.class));
+	}
+	
+	public void onStartSearch(View view){
+		startActivity(new Intent(this,SearchActivity.class));
+	}
+	
 
 }
