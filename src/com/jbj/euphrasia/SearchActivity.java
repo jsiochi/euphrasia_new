ee0e9a12b93271d5067b77cbde51787bdcb769f0
Jeremiah Siochi <jeremiahsiochi@gmail.com>
2013-11-23 23:25:24 -0500

    trying to get search to work

--- src/com/jbj/euphrasia/SearchActivity.java
+++ src/com/jbj/euphrasia/SearchActivity.java
@@ -4,7 +4,9 @@ import com.jbj.euphrasia.EntryContract.EntryColumns;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.ListActivity;
+import android.app.SearchManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,10 +28,20 @@ public class SearchActivity extends ListActivity implements android.app.LoaderMa
 	private Cursor myCursor;
 	private Controller myController;
 	
+	private static final String SELECTION_QUERY = "SelectionQuery";
+	private static final String SELECTION_ARGS = "SelectionArgs";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
+		
+//		Intent intent = getIntent();
+//		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+//			String query = intent.getStringExtra(SearchManager.QUERY);
+//			this.doSearch(query);
+//		}
+		
 		ListView listView = (ListView) findViewById(android.R.id.list);
 		//query database for collection of all tags
 		//display these tags + frequency onCreate
@@ -40,7 +52,8 @@ public class SearchActivity extends ListActivity implements android.app.LoaderMa
 //        getListView().setEmptyView(progressBar);
 //        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
 //        root.addView(progressBar);
-        getLoaderManager().initLoader(0, null, this);
+		Bundle emptyBundle = new Bundle();
+        getLoaderManager().initLoader(0, emptyBundle, this);
         //take data from columns and put in specific views
         String[] fromColumns = {EntryContract.EntryColumns.COLUMN_NAME_TITLE, EntryContract.EntryColumns.COLUMN_NAME_TAG};
         int[] toViews = {R.id.item_title, R.id.item_tags};
@@ -48,6 +61,7 @@ public class SearchActivity extends ListActivity implements android.app.LoaderMa
                 R.layout.search_list_item, myCursor,
                 fromColumns, toViews, 0);
         setListAdapter(myCursorAdapter);
+        //this.doSearch("ea");
         this.displayEverything();
 	}
 
@@ -68,7 +82,9 @@ public class SearchActivity extends ListActivity implements android.app.LoaderMa
             baseUri = EntryProvider.CONTENT_URI;
         }
         String[] projection = {EntryColumns._ID, EntryColumns.COLUMN_NAME_TITLE, EntryColumns.COLUMN_NAME_TAG};
-        return new CursorLoader(this, baseUri,projection, null, null,null);
+        String selection = arg1.getString(SELECTION_QUERY);
+        String[] selectionArgs = arg1.getStringArray(SELECTION_ARGS);
+        return new CursorLoader(this, baseUri, projection, selection, selectionArgs,null);
 	}
 
 
@@ -85,6 +101,18 @@ public class SearchActivity extends ListActivity implements android.app.LoaderMa
 	public void displayEverything(){
 		String[] projection = {EntryContract.EntryColumns.COLUMN_NAME_TITLE, EntryContract.EntryColumns.COLUMN_NAME_TAG};
 		myCursor = getContentResolver().query(EntryProvider.CONTENT_URI, projection, null,null,null);
+	}
+	
+	public void doSearch(String query) {
+		String[] projection = {EntryContract.EntryColumns.COLUMN_NAME_TITLE, EntryContract.EntryColumns.COLUMN_NAME_TAG};
+		String selection = EntryColumns.COLUMN_NAME_TAG + " LIKE '%?%' OR " + EntryColumns.COLUMN_NAME_NATIVE_TEXT + " LIKE '%?%'";
+		String[] selectionArgs = {query, query};
+		Bundle args = new Bundle();
+		Log.i("Here we go", "I AM a freaking PIZZA aaa");
+		args.putStringArray(SELECTION_ARGS, selectionArgs);
+		args.putString(SELECTION_QUERY, selection);
+		//this.getLoaderManager().restartLoader(0, args, this);
+		Log.i("Here we go", "I AM a freaking PIZZA bbb");
 	}
 
 }
