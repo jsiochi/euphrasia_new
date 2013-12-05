50738b7fdeec921a634c0463552431a2e726ac04
Jeremiah Siochi <jeremiahsiochi@gmail.com>
2013-12-04 23:42:55 -0500

    added phrasebook column

--- src/com/jbj/euphrasia/EntryDatabaseHelper.java
+++ src/com/jbj/euphrasia/EntryDatabaseHelper.java
@@ -7,10 +7,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 
 public class EntryDatabaseHelper extends SQLiteOpenHelper {
-	public static final int DATABASE_VERSION = 2;
 	public static final String DATABASE_NAME = "Entry.db";
+	public static final int DATABASE_VERSION = 4;
 	private static final String TEXT_TYPE = " TEXT";
 	private static final String COMMA_SEP = ", ";
+	private static final String NEW_COLUMNS = EntryColumns.COLUMN_NAME_PHRASEBOOK + TEXT_TYPE + ";"; //change every time new column is added
 	private static final String SQL_CREATE_ENTRIES = 
 			"CREATE TABLE " + EntryColumns.TABLE_NAME + "(" +
 			EntryColumns._ID + " INTEGER PRIMARY KEY, " +
@@ -21,8 +22,11 @@ public class EntryDatabaseHelper extends SQLiteOpenHelper {
 			EntryColumns.COLUMN_NAME_AUDIO + TEXT_TYPE + COMMA_SEP + 
 			EntryColumns.COLUMN_NAME_DATE + TEXT_TYPE + COMMA_SEP + 
 			EntryColumns.COLUMN_NAME_TAG + TEXT_TYPE + COMMA_SEP + 
+			EntryColumns.COLUMN_NAME_PHRASEBOOK + TEXT_TYPE + COMMA_SEP + 
 			EntryColumns.COLUMN_NAME_NULLABLE + TEXT_TYPE +
 			");";
+	
+	private static final String SQL_UPGRADE_TABLE = "ALTER TABLE " + EntryColumns.TABLE_NAME + " ADD COLUMN " + NEW_COLUMNS;
 	
 	private static final String SQL_DELETE_ENTRIES = 
 			"DROP TABLE IF EXISTS " + EntryColumns.TABLE_NAME;
@@ -41,6 +45,7 @@ public class EntryDatabaseHelper extends SQLiteOpenHelper {
 		// TODO: implement this with correct functionality
 		database.execSQL(SQL_DELETE_ENTRIES);
 		onCreate(database);
+		//database.execSQL(SQL_UPGRADE_TABLE);
 	}
 	
 	public void onDowngrade(SQLiteDatabase database, int oldVersion, int newVersion) {
