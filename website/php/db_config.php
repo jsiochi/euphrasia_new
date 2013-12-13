c1be986768188c861becf70f2452a9a3178d6799
Brad Sykes <bradleysykes1@gmail.com>
2013-12-12 20:23:03 -0500

    Fixed bugs in php, added toasts for debug purposes

--- website/php/db_config.php
+++ website/php/db_config.php
@@ -3,8 +3,8 @@
 db connection variables
 */
 
-define('DB_USER',"Euphrasia");
-define("DB_PASSWORD","");
+define('DB_USER',"goeuph5_brad");
+define("DB_PASSWORD","adogjumpedinthelake4567times");
 define("DB_DATABASE","goeuph5_euphrasia1");
 define("DB_SERVER","ecbiz137");
 
